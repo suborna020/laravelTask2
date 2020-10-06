c97b7f55ee572fa38a177ba2c1a5e0934f22edd2
suborna020 <suborna020@gmail.com>
2020-10-06 11:09:50 +0600

    initial release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,9 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     {{-- table design link  --}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css"> --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     {{-- myStyle sheet  --}}
     <link href="/css/mystyle.css" rel=" stylesheet">
     {{-- for check box icon --}}
@@ -42,7 +44,7 @@
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed navbar-lightblue" id="body_css">
-    <div class="wrapper">
+    <div class="wrapper ">
 
         <img src="/user.jpg" alt="Avatar" id="profile_img" style="width:40px" class="float-right" />
         <!-- /.navbar -->
@@ -56,8 +58,8 @@
             </a>
 
             <div id="center">
-                <a class="btn btn-info" href="javascript:;" data-toggle="modal" data-target="#composeModal" style="
-    border-radius:0px !important;">
+                <a class="btn btn-info" href="javascript:;" data-toggle="modal" data-target="#composeModal"
+                    style="border-radius:0px !important;">
                     Compose
                 </a>
             </div>
@@ -139,11 +141,11 @@
         </aside>
 
 
+      
 
-
-        @yield('content')
-        <!-- /.content-wrapper -->
-        <div class=" modal fade " role=" dialog" id="composeModal">
+                                @yield('content')
+                                <!-- /.content-wrapper -->
+                                <div class=" modal fade " role=" dialog" id="composeModal">
                                 <div class="modal-dialog modal-lg">
 
                                     <!-- Modal content-->
@@ -244,8 +246,20 @@
     <!-- my js file link -->
     <script src="{{url('assets/js/custom.js')}}"></script>
     {{-- table design link --}}
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js">
+    </script>
     <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    {{-- ///////////// --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    {{-- ///////////// --}}
     <script type="text/javascript">
         $(document).ready(function(){
         $('.datatable').dataTable();
