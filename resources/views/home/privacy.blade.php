fd60db149ee793d581b7c14928afed6ba01ff574
suborna020 <suborna020@gmail.com>
2020-10-17 13:00:20 +0600

    navbar correction

--- resources/views/home/privacy.blade.php
+++ resources/views/home/privacy.blade.php
@@ -3,6 +3,25 @@
 
 @section('content')
 <div class="content-wrapper" style="background-color:white;">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+
+
+        </ul>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <div class="text_white" style="font-size: 20px;">
+                    &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                </div>
+            </li>
+            <li class="nav-item">
+                <img src="../img/Group 99.svg" alt="Avatar" id="profile_img" style="width:40px" class="float-right" />
+            </li>
+        </ul>
+    </nav>
     <!-- Main content -->
     <section class="content">
         <div class="row">
@@ -59,7 +78,7 @@
                                 <label for="password" class="col-sm-11 col-form-label left_align mb-2">Change Password</label>
                                 <input type="password" class="form-control" id="Old password" placeholder="Password">
                             </div>
-                            
+
                         </form>
                     </div>
                     <!-- /.login-card-body -->
--- resources/views/home/upload.blade.php
+++ resources/views/home/upload.blade.php
@@ -2,8 +2,26 @@
 @section('title','Clients Customer Web App')
 
 @section('content')
-
 <div class="content-wrapper" style="background-color:white;">
+ <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+
+
+        </ul>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <div class="text_white" style="font-size: 20px;">
+                    &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                </div>
+            </li>
+            <li class="nav-item">
+                <img src="../img/Group 99.svg" alt="Avatar" id="profile_img" style="width:40px" class="float-right" />
+            </li>
+        </ul>
+    </nav>
     <!-- Main content -->
     <section class="content">
         <div class="row">
